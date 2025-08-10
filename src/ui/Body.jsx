8531b5a9c10335b5ae4a9546fc519578eef607df
1v4nk0 <1vancherk3s@gmail.com>
2025-08-10 13:02:02 +0100

    added pet sprite and animation

--- src/ui/Body.jsx
+++ src/ui/Body.jsx
@@ -3,9 +3,9 @@ import PetContainer from "./PetContainer";
 function Body() {
   //CONTAINER FOR THE NAME SPRITE AND STATS OF A PET
   return (
-    <div>
+    <>
       <PetContainer name="oscar" frameIndex={9} />
-    </div>
+    </>
   );
 }
 
--- src/ui/PetContainer.jsx
+++ src/ui/PetContainer.jsx
@@ -1,15 +1,22 @@
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from "react";
 import "../index.css";
 
-function Pet({ frameIndex }) {
-  const frameSize = 32;
-  const backgroundPosition = `-${frameIndex * frameSize}px 0px`;
-
+function Pet({ frameIndex, frameCount = 10 }) {
+  // Original sprite dimensions
+  const originalFrameSize = 32;
+  const originalSheetWidth = 320; // 32px * 10 frames = 320px
+  
   const style = {
     backgroundImage: "url(/Idle.png)",
-    // make sure Idle.png is in the public folder
-    backgroundPosition: backgroundPosition,
+    // Use CSS custom properties to handle the positioning and sizing
+    // This will be overridden by CSS classes for different screen sizes
+    backgroundPosition: `calc(-${frameIndex} * var(--sprite-size, 64px)) 0px`,
+    backgroundSize: `calc(var(--sprite-size, 64px) * 10) var(--sprite-size, 64px)`,
+    backgroundRepeat: "no-repeat",
+    imageRendering: "pixelated",
   };
-
+  
   return <div className="pet-sprite" style={style} />;
 }
 
@@ -17,11 +24,23 @@ function NameTag({ name }) {
   return <div className="pet-name">{name}</div>;
 }
 
-function PetContainer({ name, frameIndex }) {
+function PetContainer({ name }) {
+  const [frameIndex, setFrameIndex] = useState(0);
+  const frameCount = 10; // total frames in your sprite
+
+  useEffect(() => {
+    // Change frame every 150ms (~6-7 frames per second)
+    const interval = setInterval(() => {
+      setFrameIndex((prev) => (prev + 1) % frameCount);
+    }, 250);
+
+    return () => clearInterval(interval);
+  }, [frameCount]);
+
   return (
     <div className="pet-container">
       <NameTag name={name} />
-      <Pet frameIndex={frameIndex} />
+      <Pet frameIndex={frameIndex} frameCount={frameCount} />
     </div>
   );
 }
